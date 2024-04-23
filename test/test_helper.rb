113e8d086909905c81b2411fe11c96b5761bb8c1
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-04-24 01:04:00 +0300

    wrote test according to TDD approach

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,11 +1,14 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
+require 'bundler/setup'
+Bundler.require
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "ipgeobase"
 
 require "minitest/autorun"
 
 require "webmock/minitest"
-
-require "simplecov"
-SimpleCov.start
--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -1,37 +1,48 @@
 # frozen_string_literal: true
 
-require "test_helper"
-require "../lib/ipgeobase"
+require_relative "test_helper"
+require_relative "../lib/ipgeobase"
 
 class TestIpgeobase < Minitest::Test
   def setup
-    stub_request(:get, "http://ip-api.com/xml/8.8.8.8").
-          to_return(:body => %Q(
-    <?xml version="1.0" encoding="UTF-8"?>
-    <query>
-    <status>success</status>
-    <country>United States</country>
-    <countryCode>US</countryCode>
-    <region>VA</region>
-    <regionName>Virginia</regionName>
-    <city>Ashburn</city>
-    <zip>20149</zip>
-    <lat>39.03</lat>
-    <lon>-77.5</lon>
-    <timezone>America/New_York</timezone>
-    <isp>Google LLC</isp>
-    <org>Google Public DNS</org>
-    <as>AS15169 Google LLC</as>
-    <query>8.8.8.8</query>
-    </query>
-    ))
-  end
+    xml = %Q(
+      <query>
+        <status>success</status>
+        <country>United States</country>
+        <countryCode>US</countryCode>
+        <region>VA</region>
+        <regionName>Virginia</regionName>
+        <city>Ashburn</city>
+        <zip>20149</zip>
+        <lat>39.03</lat>
+        <lon>-77.5</lon>
+        <timezone>America/New_York</timezone>
+        <isp>Google LLC</isp>
+        <org>Google Public DNS</org>
+        <as>AS15169 Google LLC</as>
+        <query>8.8.8.8</query>
+      </query>
+      )
 
-  def test_request_was_sent
-
+    stub_request(:get, 'http://ip-api.com/xml/8.8.8.8').
+       to_return(:body => xml)
+    
+    @expected_atrributes = {
+      city: 'Ashburn',
+      country: 'United States',
+      countryCode: 'US',
+      lat: '39.03',
+      lon: '-77.5'
+    }
   end
 
   def test_gem_return_metadata
+    ip_meta = Ipgeobase.lookup '8.8.8.8'
 
+    assert { @expected_atrributes[:city] == ip_meta.city }
+    assert { @expected_atrributes[:country] == ip_meta.country }
+    assert { @expected_atrributes[:countryCode] == ip_meta.countryCode }
+    assert { @expected_atrributes[:lat] == ip_meta.lat }
+    assert { @expected_atrributes[:lon] == ip_meta.lon }
   end
 end
