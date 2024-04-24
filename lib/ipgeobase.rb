885ed4ca54254ab323725a9cc149d73ea5351c92
Khamitskiy Vladislav <vladislaviktorovic@gmail.com>
2024-04-24 21:38:55 +0300

    main gem logic

--- lib/ipgeobase.rb
+++ lib/ipgeobase.rb
@@ -1,8 +1,36 @@
 # frozen_string_literal: true
 
 require_relative "ipgeobase/version"
+require_relative "ipgeobase/ipparser"
+require "open-uri"
+require "nokogiri"
 
 module Ipgeobase
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.lookup ip
+    IpParser.new.lookup ip
+  end
+
+  Nokogiri::XML::Element.class_eval do
+    def city
+      self.xpath('city').text
+    end
+
+    def country
+      self.xpath('country').text
+    end
+
+    def countryCode
+      self.xpath('countryCode').text
+    end
+    
+    def lat
+      self.xpath('lat').text
+    end
+    
+    def lon
+      self.xpath('lon').text
+    end
+  end
 end
---
+++ lib/ipgeobase/ipparser.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Ipgeobase
+  class IpParser
+    def lookup ip
+      responce = Nokogiri::XML(URI.open("http://ip-api.com/xml/#{ip}"))
+      responce.root
+    end
+  end
+end
